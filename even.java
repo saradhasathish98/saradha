9a755211d1eb6e7ec6d4a52a8b3a5bb64d2f535c
saradhasathish98 <41267819+saradhasathish98@users.noreply.github.com>
2018-07-15 09:21:28 -0700

    Create even.java

---
+++ even.java
@@ -0,0 +1,15 @@
+public class Positive {
+public static void main(String[] args) {
+	int b=8;
+	if(b%2==0)
+	{
+		System.out.println("even");
+	}
+	else 
+	{
+		System.out.println("odd");
+	}
+
+	
+}
+}
