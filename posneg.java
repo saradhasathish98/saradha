1c6fb60bad68b1d98f8fe756e68d94dabfa59483
saradhasathish98 <41267819+saradhasathish98@users.noreply.github.com>
2018-07-15 09:15:08 -0700

    Create posneg.java

---
+++ posneg.java
@@ -0,0 +1,19 @@
+public class Positive {
+public static void main(String[] args) {
+	int b=-9;
+	if(b>0)
+	{
+		System.out.println("positive");
+	}
+	else if(b<0)
+	{
+		System.out.println("negative");
+	}
+	else 
+	{
+		System.out.println("zero");
+	}
+
+	
+}
+}
